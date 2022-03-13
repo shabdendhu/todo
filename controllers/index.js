83978b3f11517574990400add60773afd6c795b9
shabdendhu <tshabdendhu@gmail.com>
2022-03-13 20:54:00 +0530

    besic dev

--- controllers/index.js
+++ controllers/index.js
@@ -1,42 +1,81 @@
 const response = require("../response");
+const moment =require('moment')
+let data = [
+  {
+    id: 1,
+    taskName: "UX For App",
+    remaingTime: "30Days Left",
+    description: "xyz",
+    date: "10-12-2020",
+    time:"20-03 pm",
+    priority: "normal",
+  },
+  {
+    id: 2,
+    taskName: "UX For App",
+    remaingTime: "30Days Left",
+    description: "xyz",
+    date: "10-12-2020",
+    time:"20-03 pm",
+    priority: "heigh",
+  },
+  {
+    id: 3,
+    taskName: "UX For App",
+    remaingTime: "30Days Left",
+    description: "xyz",
+    date: "10-12-2020",
+    time:"20-03 pm",
+    priority: "normal",
+  },
+  {
+    id: 4,
+    taskName: "UX For App",
+    remaingTime: "30Days Left",
+    description: "xyz",
+    date: "10-12-2020",
+    time:"20-03 pm",
+    priority: "normal",
+  },
+  {
+    id: 5,
+    taskName: "UX For App",
+    remaingTime: "30Days Left",
+    description: "xyz",
+    date: "10-12-2020",
+    time:"20-03 pm",
+    priority: "heigh",
+  },
+  {
+    id: 6,
+    taskName: "UX For App",
+    remaingTime: "30Days Left",
+    description: "xyz",
+    date: "10-12-2020",
+    time:"20-03 pm",
+    priority: "low",
+  },
+  {
+    id: 7,
+    taskName: "UX For App",
+    remaingTime: "30Days Left",
+    description: "xyz",
+    date: "10-12-2020",
+    time:"20-03 pm",
+    priority: "low",
+  },
+];
 
 class Todo {
   async get_all_tasks(req, res) {
-    let data = [
-      {
-        id: 1,
-        taskName: "UX For App",
-        remaingTime: "30Days Left",
-        description: "xyz",
-        startDate: "10-12-2020",
-        endDate: "01-01-2022",
-      },
-      {
-        id: 2,
-        taskName: "UX For App",
-        remaingTime: "30Days Left",
-        description: "xyz",
-        startDate: "10-12-2020",
-        endDate: "01-01-2022",
-      },
-      {
-        id: 3,
-        taskName: "UX For App",
-        remaingTime: "30Days Left",
-        description: "xyz",
-        startDate: "10-12-2020",
-        endDate: "01-01-2022",
-      },
-      {
-        id: 4,
-        taskName: "UX For App",
-        remaingTime: "30Days Left",
-        description: "xyz",
-        startDate: "10-12-2020",
-        endDate: "01-01-2022",
-      },
-    ];
+    // data.map((e,i)=>(
+    //   e.remaingTime=moment(e.endDate).diff(e.date,'hours')
+    // ))
     res.json(response(true, "fetch successfully", data));
+  }
+  async add_new_task(req, res) {
+    data.push(req.body);
+    res.json(response(true, "added successfully", {}));
   }
 }
 module.exports = new Todo();
